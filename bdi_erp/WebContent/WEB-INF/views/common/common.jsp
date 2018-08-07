473418e1d22d6e6e9e45504b3c715c7f1318119f
KOITT <KOITT@KOITT-PC>
2018-08-07 19:25:31 +0900

    asdasdsad

---
+++ bdi_erp/WebContent/WEB-INF/views/common/common.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+String uri = request.getRequestURI();
+String rPath = request.getContextPath();
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>빅데이터반 테스트 프로젝트</title>
+<link rel = "styLesheet" href="<%=rPath %>/bs3/css/bootstrop-css">;
+<link rel = "styLesheet" href="<%=rPath %>/bs3/js/bootstrop-js">;
+<script type="<%=rPath %>/bs3/css/bootstrop-css"></script>
+<script type="<%=rPath %>/bs3/js/bootstrop-js"></script>
+</head>
---
+++ bdi_erp/src/com/bdi/erp/servlet/ViewServlet.java
@@ -0,0 +1,31 @@
+package com.bdi.erp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String rPath = request.getContextPath();
+		//uri = uri.replace(arg0, arg1);
+		uri="/WEB-INF" + uri + ".jsp";
+		//System.out.println("3.난 계속 나와!");
+		//String uri= "/WEB-INF/views/user/userList.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(uri);
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
